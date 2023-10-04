5db9b852369b50034d7684d9a4e61107f6a0c244
Adam M <whiskypo082@gmail.com>
2023-10-03 22:37:26 -0500

    Time Error is section 13

--- frontend/web-app/app/auctions/CountdownTimer.tsx
+++ frontend/web-app/app/auctions/CountdownTimer.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { useBidStore } from '@/hooks/useBidStore';
+import { usePathname } from 'next/navigation';
 import React from 'react'
 import Countdown, { zeroPad } from 'react-countdown';
 
@@ -7,10 +9,10 @@ type Props = {
     auctionEnd: string;
 }
 
-const renderer = ({ days, hours, minutes, seconds, completed }: 
-    {days: number, hours: number, minutes: number, seconds: number, completed: boolean}) => {
-        return (
-            <div className={`
+const renderer = ({ days, hours, minutes, seconds, completed }:
+    { days: number, hours: number, minutes: number, seconds: number, completed: boolean }) => {
+    return (
+      <div className={`
                 border-2 
                 border-white 
                 text-white py-1 px-2 
@@ -19,22 +21,30 @@ const renderer = ({ days, hours, minutes, seconds, completed }:
                     'bg-red-600' : (days === 0 && hours < 10) 
                     ? 'bg-amber-600' : 'bg-green-600'}
             `}>
-                  {completed ? (
-                    <span>Auction finished</span>
-                  ) : (
-                    <span suppressHydrationWarning={true}>
-                        {zeroPad(days)}:{zeroPad(hours)}:{zeroPad(minutes)}:{zeroPad(seconds)}
-                    </span>
-                  )}  
-            </div>
+            {completed ? (
+              <span>Auction finished</span>
+            ) : (
+              <span suppressHydrationWarning={true}>
+                {zeroPad(days)}:{zeroPad(hours)}:{zeroPad(minutes)}:{zeroPad(seconds)}
+              </span>
+            )}
+          </div>
         )
-  };
+      };
 
 
-export default function CountdownTimer({auctionEnd}: Props) {
+  export default function CountdownTimer({ auctionEnd }: Props) {
+    const setOpen = useBidStore(state => state.setOpen);
+    const pathname = usePathname();
+  
+    function auctionFinished() {
+      if (pathname.startsWith('/auctions/details')) {
+        setOpen(false)
+      }
+    }
   return (
     <div>
-        <Countdown date={auctionEnd} renderer={renderer} />
+       <Countdown date={auctionEnd} renderer={renderer} onComplete={auctionFinished} />
     </div>
   )
 }
